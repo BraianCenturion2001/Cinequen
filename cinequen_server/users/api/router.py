285c73087f8ebdd90837c71824967676de977108
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-09-20 17:25:27 -0300

    actualizar contraseña la encripta y ruta para recuperar datos del user logeado

--- cinequen_server/users/api/router.py
+++ cinequen_server/users/api/router.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from users.api.views import UserApiViewSet
+from users.api.views import UserApiViewSet, UserView
 
 router_user = DefaultRouter()
 
@@ -8,3 +8,6 @@ router_user.register(
     prefix='users', basename='users', viewset=UserApiViewSet
 )
 
+urlpatterns = [
+    path('auth/me/', UserView.as_view())
+]
--- cinequen_server/users/api/views.py
+++ cinequen_server/users/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
 from users.models import User
-from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from users.api.serializers import UserSerializer
 from django.contrib.auth.hashers import make_password
 
@@ -13,3 +15,19 @@ class UserApiViewSet(ModelViewSet):
     def create(self, request, *args, **kwargs):
         request.data['password'] = make_password(request.data['password'])
         return super().create(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        password = request.data['password']
+        if password:
+            request.data['password'] = make_password(password)
+        else:
+            request.data['password'] = request.user.password
+        return super().update(request, *args, **kwargs)
+
+
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serialiazer = UserSerializer(request.user)
+        return Response(serialiazer.data)
