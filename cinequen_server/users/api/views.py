e6f19624573c687ce28f202bc27a0089d33026b7
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-09-20 17:08:45 -0300

    urls para user y encriptacion al guardar user

---
+++ cinequen_server/users/api/views.py
@@ -0,0 +1,15 @@
+from rest_framework.viewsets import ModelViewSet
+from users.models import User
+from rest_framework.permissions import IsAdminUser
+from users.api.serializers import UserSerializer
+from django.contrib.auth.hashers import make_password
+
+
+class UserApiViewSet(ModelViewSet):
+    permission_classes = [IsAdminUser]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        request.data['password'] = make_password(request.data['password'])
+        return super().create(request, *args, **kwargs)
