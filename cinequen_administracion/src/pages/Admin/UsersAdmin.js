7a1a62ac44c24ca6c6f475db2a4a3b9208664b4e
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-09-23 14:01:02 -0300

    MODAL BASICO DINAMICO

--- cinequen_administracion/src/pages/Admin/UsersAdmin.js
+++ cinequen_administracion/src/pages/Admin/UsersAdmin.js
@@ -1,19 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { HeaderPage, TableUsers } from "../../components/Admin"
+import { ModalBasic } from "../../components/Common"
 import { useUser } from "../../hooks"
 import { Loader } from "semantic-ui-react"
 
+
 export function UsersAdmin() {
     const { loading, users, getUsers } = useUser();
+    const [showModal, setShowModal] = useState(false)
+    const [titleModal, setTitleModal] = useState(null)
+    const [contentModal, setContentModal] = useState(null)
 
     useEffect(() => {
         getUsers();
     }, [])
 
+    const openCloseModal = () => setShowModal((prev) => !prev);
+
+    const addUser = () => {
+        setTitleModal("Nuevo Usuario");
+        setContentModal(<h2>Crear usuario</h2>);
+        openCloseModal();
+    }
 
     return (
         <>
-            <HeaderPage title="Usuarios" btnTitle="Nuevo Usuario" />
+            <HeaderPage title="Usuarios" btnTitle="Nuevo Usuario" btnClick={addUser} />
 
             {loading ? (
                 <Loader active inline="centered">
@@ -22,6 +34,8 @@ export function UsersAdmin() {
             ) : (
                 <TableUsers users={users} />
             )}
+
+            <ModalBasic show={showModal} onClose={openCloseModal} title={titleModal} children={contentModal} />
         </>
     )
 }
