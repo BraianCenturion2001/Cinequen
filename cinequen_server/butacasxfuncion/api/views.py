c966b2d5c433517fa9eb187435fe5ac7be86d209
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-10-16 18:51:06 -0300

    funcion cuando se crea genera los butacaxfuncion correspondientes

--- cinequen_server/butacasxfuncion/api/views.py
+++ cinequen_server/butacasxfuncion/api/views.py
@@ -3,13 +3,13 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 
 from butacasxfuncion.models import ButacaxFuncion
-from butacasxfuncion.api.serializer import ButacaSerializer
+from butacasxfuncion.api.serializer import ButacaxFuncionSerializer
 from butacasxfuncion.api.filters import ButacaxFuncionFilter
 
 
 class ButacaxFuncionApiViewSet(ModelViewSet):
     permissions_classes = [IsAuthenticatedOrReadOnly]
-    serializer_class = ButacaSerializer
+    serializer_class = ButacaxFuncionSerializer
     queryset = ButacaxFuncion.objects.all()
     filter_backends = [DjangoFilterBackend]
     filterset_class = ButacaxFuncionFilter
--- cinequen_server/funciones/api/views.py
+++ cinequen_server/funciones/api/views.py
@@ -6,6 +6,12 @@ from funciones.models import Funcion
 from funciones.api.serializer import FuncionSerializer
 from funciones.api.filters import FuncionFilter
 
+from butacas.models import Butaca
+from butacasxfuncion.models import ButacaxFuncion
+
+from rest_framework.response import Response
+from rest_framework import status
+
 
 class FuncionApiViewSet(ModelViewSet):
     permissions_classes = [IsAuthenticatedOrReadOnly]
@@ -13,3 +19,24 @@ class FuncionApiViewSet(ModelViewSet):
     queryset = Funcion.objects.all()
     filter_backends = [DjangoFilterBackend]
     filterset_class = FuncionFilter
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # Guardar la función
+        self.perform_create(serializer)
+
+        # Obtener la sala de la función creada
+        sala = serializer.validated_data['sala']
+
+        # Obtener todas las butacas de la sala
+        butacas = Butaca.objects.filter(sala=sala)
+
+        # Crear un ButacaxFuncion para cada butaca
+        for butaca in butacas:
+            ButacaxFuncion.objects.create(
+                butaca=butaca, funcion=serializer.instance)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
