37b552e9d7c8799ca0cf71dced7e01c5ca44073a
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-10-17 16:04:28 -0300

    modificacion butacasxfuncion no trae objetos

--- cinequen_server/butacasxfuncion/api/serializer.py
+++ cinequen_server/butacasxfuncion/api/serializer.py
@@ -1,15 +1,9 @@
 from rest_framework.serializers import ModelSerializer
 
-from butacas.api.serializer import ButacaSerializer
-from funciones.api.serializer import FuncionSerializer
 from butacasxfuncion.models import ButacaxFuncion
 
 
 class ButacaxFuncionSerializer(ModelSerializer):
-    butaca_data = ButacaSerializer(
-        source='butaca', read_only=True)
-    funcion_data = FuncionSerializer(
-        source='funcion', read_only=True)
 
     class Meta:
         model = ButacaxFuncion
@@ -17,7 +11,5 @@ class ButacaxFuncionSerializer(ModelSerializer):
             'id',
             'estado',
             'butaca',
-            'butaca_data',
             'funcion',
-            'funcion_data',
         ]
