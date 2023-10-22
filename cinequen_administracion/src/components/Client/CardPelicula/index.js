1b98eb024cef98aa4c6f1ab961e6621d17f16a27
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-10-22 00:03:54 -0300

    card para ver informacion rapido sobre la pelicula y funcion

--- cinequen_administracion/src/components/Client/CardPelicula/index.js
+++ cinequen_administracion/src/components/Client/CardPelicula/index.js
@@ -1,1 +1,2 @@
 export * from "./CardPelicula"
+export * from "./CardPelicula2"
--- cinequen_administracion/src/components/Client/FormCompraEntradas/Paso1Entradas.js
+++ cinequen_administracion/src/components/Client/FormCompraEntradas/Paso1Entradas.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { TextField } from "@mui/material"
+import { CardPelicula2 } from '../CardPelicula';
 
 export function Paso1Entradas(props) {
     const { cantidadEntradas, setCantidadEntradas, precioEntradas, setPrecioEntradas, funcion } = props;
     return (
-        <div>Paso1Entradas</div>
+        <>
+            <CardPelicula2 funcion={funcion} />
+            <TextField
+                id="outlined-number"
+                label="Cantidad de Entradas"
+                type="number"
+            />
+        </>
     )
 }
