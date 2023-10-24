79d9a39ff37e9aba79e38d0941ee05e202f16d5f
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-10-24 14:25:56 -0300

    alta de entrada actualiza butacas

--- cinequen_server/entradas/api/views.py
+++ cinequen_server/entradas/api/views.py
@@ -6,6 +6,11 @@ from entradas.models import Entrada
 from entradas.api.serializer import EntradaClienteSerializer, EntradaSerializer, RegistroEntradaSerializer
 from entradas.api.filter import EntradaFilter
 
+from butacasxfuncion.models import ButacaxFuncion
+
+from rest_framework.response import Response
+from rest_framework import status
+
 
 class EntradaApiViewSet(ModelViewSet):
     permissions_classes = [IsAuthenticatedOrReadOnly]
@@ -20,3 +25,20 @@ class EntradaApiViewSet(ModelViewSet):
     queryset = Entrada.objects.all()
     filter_backends = [DjangoFilterBackend]
     filterset_class = EntradaFilter
+
+    def perform_create(self, serializer):
+        entrada = serializer.save()  # Guardar la nueva instancia de Entrada
+        ids_butacas = entrada.idsButacasxFuncion.split(
+            ',')  # Obtener los IDs de butacas
+
+        # Actualizar el estado de las butacas
+        for id_butaca in ids_butacas:
+            try:
+                butaca_x_funcion = ButacaxFuncion.objects.get(id=id_butaca)
+                butaca_x_funcion.estado = 1  # Actualizar el estado a 1
+                butaca_x_funcion.save()
+            except ButacaxFuncion.DoesNotExist:
+                # Manejar el caso de que no se encuentre la butaca por el ID
+                pass
+
+        return Response(status=status.HTTP_201_CREATED)
