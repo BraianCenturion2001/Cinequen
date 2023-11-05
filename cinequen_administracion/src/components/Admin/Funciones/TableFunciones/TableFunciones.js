1841a61b9394a9004bb474145a7fc56bd2c96011
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-11-05 20:52:23 -0300

    nueva tabla seccion funciones

--- cinequen_administracion/src/components/Admin/Funciones/TableFunciones/TableFunciones.js
+++ cinequen_administracion/src/components/Admin/Funciones/TableFunciones/TableFunciones.js
@@ -1,51 +1,75 @@
 import React from 'react'
 import "./TableFunciones.scss"
-import { Table } from "semantic-ui-react"
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import { ButtonDelete, ButtonEdit } from "../../Buttons"
 import { map } from "lodash"
+
+
+const Actions = (props) => {
+    const { funcion } = props;
+    return (
+        <>
+            <ButtonEdit objeto={funcion} />
+            <ButtonDelete objeto={funcion} />
+        </>
+    );
+};
+
+const convertirFormatoHora = (hora) => {
+    if (hora && hora.includes(':')) {
+        const [horas, minutos, segundos] = hora.split(':');
+        const horasFormato12 = parseInt(horas, 10) % 12 || 12;
+        return `${horasFormato12}:${minutos}hs`;
+    }
+    return '';
+};
+
+const columns: GridColDef[] = [
+    { field: 'fecha', headerName: 'Fecha', width: 200, disableColumnMenu: true, },
+    {
+        field: 'hora_inicio', headerName: 'Horario', width: 130, disableColumnMenu: true,
+        renderCell: (params: GridValueGetterParams) => (
+            <span>{convertirFormatoHora(params.value)} a {convertirFormatoHora(params.row.acciones.hora_fin)}</span>
+        ),
+    },
+    {
+        field: 'sala_data.nombre', headerName: 'Sala/Establecimiento', type: 'number', width: 240, disableColumnMenu: true,
+        renderCell: (params: GridValueGetterParams) => (
+            <span>{params.value} - {params.row.acciones.sala_data.establecimiento_data.nombre}</span>
+        ),
+    },
+    { field: 'pelicula_data.nombre', headerName: 'Película', width: 200, disableColumnMenu: true, },
+    {
+        field: 'acciones',
+        headerName: 'Acciones',
+        width: 100,
+        align: 'center',
+        disableColumnMenu: true,
+        renderCell: (params: GridValueGetterParams) => (
+            < Actions funcion={params.row} />
+        ),
+    }
+];
 
 export function TableFunciones(props) {
     const { funciones } = props;
 
-    const convertirFormatoHora = (hora) => {
-        const [horas, minutos, segundos] = hora.split(':');
-        const horasFormato12 = parseInt(horas, 10) % 12 || 12;
-        return `${horasFormato12}:${minutos}hs`;
-    };
+    const rows = map(funciones, (funcion, index) => ({
+        id: funcion.id,
+        fecha: funcion.fecha,
+        hora_inicio: funcion.hora_inicio,
+        'sala_data.nombre': funcion.sala_data.nombre,
+        'pelicula_data.nombre': funcion.pelicula_data.nombre,
+        acciones: funcion,
+    }));
 
     return (
-        <Table className='table-funciones-admin'>
-            <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell>Fecha</Table.HeaderCell>
-                    <Table.HeaderCell>Horario</Table.HeaderCell>
-                    <Table.HeaderCell>Sala</Table.HeaderCell>
-                    <Table.HeaderCell>Pelicula</Table.HeaderCell>
-                    <Table.HeaderCell>Acciones</Table.HeaderCell>
-                </Table.Row>
-            </Table.Header>
-            <Table.Body>
-                {map(funciones, (funcion, index) => (
-                    <Table.Row key={index}>
-                        <Table.Cell>{funcion.fecha}</Table.Cell>
-                        <Table.Cell>{convertirFormatoHora(funcion.hora_inicio)} a {convertirFormatoHora(funcion.hora_fin)}</Table.Cell>
-                        <Table.Cell>{funcion.sala_data.nombre} - {funcion.sala_data.establecimiento_data.nombre}</Table.Cell>
-                        <Table.Cell>{funcion.pelicula_data.nombre}</Table.Cell>
-                        <Actions funcion={funcion} /* updateSala={updateSala} deleteSala={deleteSala} */ />
-                    </Table.Row>
-                ))}
-            </Table.Body>
-        </Table>
-    )
-}
-
-function Actions(props) {
-    const { funcion, /* updateSala, deleteSala  */ } = props;
-
-    return (
-        <Table.Cell>
-            <ButtonEdit /* funcion={updateSala} */ objeto={funcion} />
-            <ButtonDelete /* funcion={deleteSala} */ objeto={funcion} />
-        </Table.Cell>
-    )
+        <div style={{ width: '100%' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={5}
+            />
+        </div>
+    );
 }
