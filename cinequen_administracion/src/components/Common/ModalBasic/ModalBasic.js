68fb2f3420ba095aa374726c504b544e9bd7420d
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-11-11 10:46:03 -0300

    Modal Actualizado

--- cinequen_administracion/src/components/Common/ModalBasic/ModalBasic.js
+++ cinequen_administracion/src/components/Common/ModalBasic/ModalBasic.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import "./ModalBasic.scss"
-import { Modal } from "semantic-ui-react"
+import { Dialog, DialogContent, DialogContentText, DialogTitle, Slide } from '@mui/material';
+import { TransitionProps } from '@mui/material/transitions';
+
+const Transition = React.forwardRef(function Transition(
+    props: TransitionProps & {
+        children: React.ReactElement<any, any>;
+    },
+    ref: React.Ref<unknown>,
+) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export function ModalBasic(props) {
-    const { show, size, title, children, onClose } = props;
+    const { show, title, children, onClose } = props;
 
     return (
-        <Modal className='modal-basic' open={show} onClose={onClose} size={size}>
-            {title && <Modal.Header>{title}</Modal.Header>}
-            <Modal.Content>{children}</Modal.Content>
-        </Modal>
+        < Dialog open={show} TransitionComponent={Transition} fullWidth={true} onClose={onClose}>
+            {title && <DialogTitle>{title}</DialogTitle>}
+            <DialogContent>
+                <DialogContentText id="alert-dialog-slide-description">
+                    {children}
+                </DialogContentText>
+            </DialogContent>
+        </Dialog >
     )
-}
-
-ModalBasic.defaultProps = {
-    size: 'tiny',
 }
