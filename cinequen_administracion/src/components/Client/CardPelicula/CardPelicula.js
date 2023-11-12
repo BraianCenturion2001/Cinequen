087588c39c014680c84b11d730eadb10e6a9e684
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-11-12 15:15:51 -0300

    Nuevo Card Pelicula

--- cinequen_administracion/src/components/Client/CardPelicula/CardPelicula.js
+++ cinequen_administracion/src/components/Client/CardPelicula/CardPelicula.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./CardPelicula.scss";
 import { Link } from 'react-router-dom';
-import { Card, CardMedia, CardContent, CardActionArea, Typography, Box } from '@mui/material';
+import { Card, CardActionArea, Typography, ImageListItem, ImageListItemBar, IconButton } from '@mui/material';
 
 export function CardPelicula(props) {
     const { pelicula } = props;
@@ -9,20 +9,29 @@ export function CardPelicula(props) {
     return (
         <Card elevation={3}>
             <CardActionArea component={Link} to={`/funciones/pelicula/${pelicula.id}`}>
-                <CardMedia component="img" height="300" image={pelicula.poster} alt={pelicula.nombre} />
-                <CardContent>
-                    <Box display="flex" alignItems="center">
-                        <h2><i class="fa-duotone fa-film"></i></h2>
-                        <Box ml={1}>
-                            <Typography component="div" variant="h5">
-                                {pelicula.nombre}
+                <ImageListItem style={{ height: '315px' }}>
+                    <img
+                        srcSet={pelicula.poster}
+                        src={pelicula.poster}
+                        alt={pelicula.nombre}
+                        loading="lazy"
+                    />
+                    <ImageListItemBar
+                        title={pelicula.nombre}
+                        subtitle={
+                            <Typography component="div">
+                                <i className="fa-duotone fa-clock-rotate-left" style={{ marginRight: '5px', '--fa-primary-color': '#ffffff', '--fa-secondary-color': '#ffffff' }}></i>
+                                Duración: {formatDuracion(pelicula.duracion)}
                             </Typography>
-                            <Typography variant="subtitle1" color="text.secondary" component="div">
-                                <i class="fa-duotone fa-clock-rotate-left" style={{ marginRight: '5px' }}></i>{`Duración: ${formatDuracion(pelicula.duracion)}`}
-                            </Typography>
-                        </Box>
-                    </Box>
-                </CardContent>
+                        } actionIcon={
+                            <IconButton
+                                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                            >
+                                <i class="fa-duotone fa-film fa-xl" style={{ '--fa-primary-color': '#ffffff', '--fa-secondary-color': '#ffffff' }}></i>
+                            </IconButton>
+                        }
+                    />
+                </ImageListItem>
             </CardActionArea>
         </Card>
     );
