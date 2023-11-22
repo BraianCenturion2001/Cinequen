acd211a9b54b336395753caca054e1483a4d67ec
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-11-21 23:37:44 -0300

    validacion por si la peli no tiene funciones

--- cinequen_administracion/src/pages/Client/FuncionesPelicula.js
+++ cinequen_administracion/src/pages/Client/FuncionesPelicula.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useFuncion } from "../../hooks";
 import { useParams } from 'react-router-dom';
-import { Icon } from 'semantic-ui-react';
-import { Button, Box } from '@mui/material';
+import { Icon, Loader } from 'semantic-ui-react';
+import { Button, Box, Typography } from '@mui/material';
 import { AccordionFunciones } from "../../components/Client"
 import { map, uniqBy } from 'lodash';
 import dayjs from 'dayjs';
@@ -25,15 +25,16 @@ export function FuncionesPelicula() {
 
     useEffect(() => {
         if (!loading) {
-            //console.log(funciones)
-            setNombrePelicula(funciones[0].pelicula_data.nombre);
-            const gruposOrdenados = ordenarFuncionesPorFechaYEstablecimiento(funciones);
-            console.log(gruposOrdenados)
-            setGruposFunciones(gruposOrdenados);
+            if (funciones.length > 0) {
+                //console.log(funciones)
+                setNombrePelicula(funciones[0].pelicula_data.nombre);
+                const gruposOrdenados = ordenarFuncionesPorFechaYEstablecimiento(funciones);
+                setGruposFunciones(gruposOrdenados);
 
-            // Seleccionar la primera fecha y mostrar el acordeón correspondiente
-            const primeraFecha = uniqBy(funciones, 'fecha')[0].fecha;
-            handleFechaClick(primeraFecha); // Seleccionar la primera fecha al cargar el componente
+                // Seleccionar la primera fecha y mostrar el acordeón correspondiente
+                const primeraFecha = uniqBy(funciones, 'fecha')[0].fecha;
+                handleFechaClick(primeraFecha); // Seleccionar la primera fecha al cargar el componente
+            }
         }
     }, [loading, funciones]);
 
@@ -88,32 +89,44 @@ export function FuncionesPelicula() {
     return (
         <>
             <Box sx={{ width: '95%', margin: '0 auto', backgroundColor: 'rgba(255, 255, 255, 0.8)', borderRadius: 1, mt: '20px', mb: '200px', padding: '30px' }}>
-                <h1>Funciones para {nombrePelicula}</h1>
-                <Box>
-                    {fechasDisponibles.map((fecha) => (
-                        <Button
-                            key={fecha}
-                            size="large"
-                            onClick={() => handleFechaClick(fecha)}
-                            variant={fecha === selectedFecha ? 'contained' : 'outlined'}
-                            sx={{
-                                margin: '1rem',
-                                alignItems: 'center',
-                            }}
-                        >
-                            <Icon name='calendar alternate outline' size='big' /> {fecha}
-                        </Button>
-                    ))}
-                </Box>
-                <Box sx={{ width: '500px' }}>
-                    {selectedFecha && (
-                        <AccordionFunciones
-                            handleChange={handleChange}
-                            expanded={expanded}
-                            gruposFunciones={gruposFunciones.find((grupo) => grupo.fecha === selectedFecha).grupos}
-                        />
-                    )}
-                </Box>
+                {loading ? (
+                    <Loader active inline="centered">
+                        Cargando
+                    </Loader>
+                ) : (
+                    funciones.length > 0 ? (
+                        <>
+                            <Typography variant="h3" align="center">Funciones para {nombrePelicula}</Typography>
+                            <Box>
+                                {fechasDisponibles.map((fecha) => (
+                                    <Button
+                                        key={fecha}
+                                        size="large"
+                                        onClick={() => handleFechaClick(fecha)}
+                                        variant={fecha === selectedFecha ? 'contained' : 'outlined'}
+                                        sx={{
+                                            margin: '1rem',
+                                            alignItems: 'center',
+                                        }}
+                                    >
+                                        <Icon name='calendar alternate outline' size='big' /> {fecha}
+                                    </Button>
+                                ))}
+                            </Box>
+                            <Box sx={{ width: '500px' }}>
+                                {selectedFecha && (
+                                    <AccordionFunciones
+                                        handleChange={handleChange}
+                                        expanded={expanded}
+                                        gruposFunciones={gruposFunciones.find((grupo) => grupo.fecha === selectedFecha).grupos}
+                                    />
+                                )}
+                            </Box>
+                        </>
+                    ) : (
+                        <Typography variant="h3" align="center">Sin funciones disponibles para esta semana.</Typography>
+                    )
+                )}
             </Box>
         </>
     );
