e538bd6fbf46ec48dea7782b49df9567a386d978
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-11-22 20:00:26 -0300

    ApiView Canje hace validaciones antes de registrar

--- cinequen_server/canjes/api/views.py
+++ cinequen_server/canjes/api/views.py
@@ -1,10 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 
 from canjes.models import Canje
 from canjes.api.filters import CanjeFilter
 from canjes.api.serializer import CanjeCompletoSerializer, CanjeSimpleSerializer, CanjeProductoSerializer
+
+from users.models import Cliente
+from productos_canje.models import ProductoCanje
 
 
 class CanjeApiViewSet(ModelViewSet):
@@ -20,3 +26,32 @@ class CanjeApiViewSet(ModelViewSet):
         elif self.request.query_params.get('cliente') is not None:
             return CanjeProductoSerializer
         return CanjeCompletoSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # Buscar el objeto Cliente correspondiente
+        cliente = Cliente.objects.get(user_id=request.data.get('cliente'))
+        # Restar los puntos_restados del serializer a los puntos del cliente
+        puntos_restados = serializer.validated_data['puntos_restados']
+        if cliente.puntos < puntos_restados:
+            raise ValidationError('Puntos insuficientes')
+
+        # Buscar el producto de canje correspondiente
+        producto = ProductoCanje.objects.get(id=request.data.get('producto'))
+        if producto.stock < 1:
+            raise ValidationError('Producto agotado')
+
+        # Restar los puntos_restados del serializer a los puntos del cliente
+        cliente.puntos -= puntos_restados
+        cliente.save()
+
+        # Restar 1 al stock del producto
+        producto.stock -= 1
+        producto.save()
+
+        # Crear el objeto Canje
+        self.perform_create(serializer)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
