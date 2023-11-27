449ba107697a5360059ffc89112b25f683cb4668
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-11-27 00:16:18 -0300

    Pelicula almacena imagen con nombre unico

--- cinequen_server/peliculas/models.py
+++ cinequen_server/peliculas/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from datetime import datetime
+import os
+
+
+def get_file_path(instance, filename):
+    ext = str(filename.split('.')[-1])
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S%f")
+    filename = "pelicula_{}.{}".format(timestamp, ext)
+    return os.path.join('poster_peliculas/', filename)
 
 
 class Pelicula(models.Model):
     nombre = models.CharField(max_length=100)
     duracion = models.IntegerField()
-    poster = models.ImageField(upload_to='poster_peliculas')
+    poster = models.ImageField(upload_to=get_file_path, blank=True)
     video_trailer = models.CharField(max_length=100)
     clasificacion = models.CharField(max_length=100)
     actores = models.CharField(max_length=250)
