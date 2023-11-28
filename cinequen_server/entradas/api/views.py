30f32794079574c30fa295665589e8a68ce58324
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-11-28 14:02:28 -0300

    cambio ip

--- cinequen_server/entradas/api/views.py
+++ cinequen_server/entradas/api/views.py
@@ -1,5 +1,4 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 
@@ -48,7 +47,7 @@ class EntradaApiViewSet(ModelViewSet):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         entrada = self.perform_create(serializer)
-        qr_value = f"http://192.168.100.7:8000/api/entradas/{entrada.pk}/"
+        qr_value = f"http://192.168.0.198:8000/api/entradas/{entrada.pk}/"
         headers = self.get_success_headers(serializer.data)
         return Response({'qr_value': qr_value}, status=status.HTTP_201_CREATED, headers=headers)
 
