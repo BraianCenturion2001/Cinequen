e8ffe1a59dbb15c0acdf5ce363e27395527be65c
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-11-28 14:02:02 -0300

    Historial de Canjes base

--- cinequen_administracion/src/pages/Client/Canjes.js
+++ cinequen_administracion/src/pages/Client/Canjes.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Loader } from "semantic-ui-react"
+import { useCanjes, useAuth } from "../../hooks"
+import { TablaCanjes } from "../../components/Client"
 
 export function Canjes() {
+    const { canjes, getCanjes, loading } = useCanjes();
+    const { auth } = useAuth();
+
+    useEffect(() => {
+        const userId = auth.me.user_id;
+        getCanjes({ user: userId });
+    }, []);
+
     return (
-        <div>Mis Canjes</div>
+        <>
+            {
+                loading ? (
+                    <Loader active inline="centered" >
+                        Cargando
+                    </Loader>
+                ) : (
+                    <TablaCanjes canjes={canjes} />
+                )
+            }
+        </>
     )
 }
