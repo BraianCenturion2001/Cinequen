0f3b1ec0b4ee07b5865bbc1e75492ec158bc91d7
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-12-04 20:07:24 -0300

    select funciones home animaciones

--- cinequen_administracion/src/components/Client/SelectFunciones/SelectFunciones.js
+++ cinequen_administracion/src/components/Client/SelectFunciones/SelectFunciones.js
@@ -1,86 +1,135 @@
-import React, { useEffect, useState } from 'react'
-import "./SelectFunciones.scss"
-import { useEstablecimiento, usePeliculaEstablecimiento, usePelicula } from "../../../hooks"
-import { Grid, Box, CircularProgress } from '@mui/material'
-import { CardPelicula } from "../../../components/Client"
-import { Dropdown } from 'semantic-ui-react';
-import { map } from 'lodash';
+import React, { useEffect, useState } from "react";
+import "./SelectFunciones.scss";
+import {
+  useEstablecimiento,
+  usePeliculaEstablecimiento,
+  usePelicula,
+} from "../../../hooks";
+import { Grid, Box, CircularProgress, Zoom, Typography } from "@mui/material";
+import { CardPelicula } from "../../../components/Client";
+import { Dropdown } from "semantic-ui-react";
+import { map } from "lodash";
 
 export function SelectFunciones() {
-    const [establecimientosFormato, setEstablecimientosFormato] = useState([]);
-    const [peliculasOptions, setPeliculasOptions] = useState([]);
+  const [checkedPeliculas, setCheckedPeliculas] = useState(false);
+  const [delay, setDelay] = useState(0);
+  const [establecimientosFormato, setEstablecimientosFormato] = useState([]);
+  const [peliculasOptions, setPeliculasOptions] = useState([]);
 
-    const { establecimientos, getEstablecimientos } = useEstablecimiento();
-    const { loading: loadingPeliculas, peliculas, getPeliculas } = usePelicula();
-    const { loading: loadingPE, peliculasEstablecimientos, getPEFiltro1, getPeliculasEstablecimiento } = usePeliculaEstablecimiento();
+  const { establecimientos, getEstablecimientos } = useEstablecimiento();
+  const { loading: loadingPeliculas, peliculas, getPeliculas } = usePelicula();
+  const {
+    loading: loadingPE,
+    peliculasEstablecimientos,
+    getPeliculasEstablecimiento,
+  } = usePeliculaEstablecimiento();
 
-    useEffect(() => {
-        getEstablecimientos()
-        getPeliculas()
-    }, [])
+  useEffect(() => {
+    getEstablecimientos();
+    getPeliculas();
+  }, []);
 
-    useEffect(() => {
-        setEstablecimientosFormato(formatDropdownData(establecimientos))
-    }, [establecimientos]);
+  useEffect(() => {
+    setEstablecimientosFormato(formatDropdownData(establecimientos));
+  }, [establecimientos]);
 
-    useEffect(() => {
-        if (!loadingPeliculas) {
-            setPeliculasOptions(peliculas)
-        }
-    }, [loadingPeliculas, peliculas]);
+  useEffect(() => {
+    if (!loadingPeliculas) {
+      setPeliculasOptions(peliculas);
+      setTimeout(() => {
+        setCheckedPeliculas(true);
+      }, 1000);
+    }
+  }, [loadingPeliculas, peliculas]);
 
-    useEffect(() => {
-        if (!loadingPE) {
-            const peliculasData = peliculasEstablecimientos.map((item) => item.pelicula_data);
-            setPeliculasOptions(peliculasData);
-        }
-    }, [loadingPE, peliculasEstablecimientos]);
+  useEffect(() => {
+    if (!loadingPE) {
+      const peliculasData = peliculasEstablecimientos.map(
+        (item) => item.pelicula_data
+      );
+      setPeliculasOptions(peliculasData);
+    }
+  }, [loadingPE, peliculasEstablecimientos]);
 
-    const handleEstablecimientoChange = async (event, data) => {
-        setPeliculasOptions([]);
-        try {
-            await getPeliculasEstablecimiento({ establecimiento: data.value });
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const handleEstablecimientoChange = async (event, data) => {
+    setPeliculasOptions([]);
+    try {
+      await getPeliculasEstablecimiento({ establecimiento: data.value });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return (
-        <>
-            <Box sx={{ width: '250px', margin: '30px' }}>
-                <Dropdown
-                    placeholder='Seleccione un Establecimiento'
-                    fluid selection search
-                    options={establecimientosFormato}
-                    onChange={handleEstablecimientoChange}
-                />
-            </Box>
-
-            {
-                loadingPeliculas ? (
-                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-                        <CircularProgress color="secondary" />
-                    </Box>
-                ) : (
-                    <Box sx={{ margin: '0 40px' }}>
-                        <Grid container spacing={12}>
-                            {peliculasOptions.map((pelicula) => (
-                                <Grid item xs={3} sm={3} md={3} lg={3} xl={3} key={pelicula.id}>
-                                    <CardPelicula pelicula={pelicula} />
-                                </Grid>
-                            ))}
-                        </Grid>
-                    </Box>
-                )
-            }
-        </>
-    )
+  return (
+    <Box
+      sx={{
+        minHeight: "500px",
+      }}
+    >
+      <Box
+        sx={{
+          width: "95%",
+          margin: "0 auto",
+          backgroundColor: "rgba(255, 255, 255, 0.8)",
+          borderRadius: 1,
+          mt: "20px",
+          mb: "20px",
+          padding: "15px",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          variant="h5"
+          gutterBottom
+          sx={{ marginRight: "15px", paddingTop: "8px" }}
+        >
+          Filtra por Establecimiento:
+        </Typography>
+        <Dropdown
+          placeholder="Seleccione un Establecimiento"
+          selection
+          search
+          options={establecimientosFormato}
+          onChange={handleEstablecimientoChange}
+        />
+      </Box>
+      {loadingPeliculas ? (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <CircularProgress color="secondary" />
+        </Box>
+      ) : (
+        <Box sx={{ margin: "0 40px" }}>
+          <Grid container spacing={12}>
+            {peliculasOptions.map((pelicula, index) => (
+              <Zoom
+                in={checkedPeliculas}
+                style={{ transitionDelay: `${delay + index * 300}ms` }}
+              >
+                <Grid item xs={3} sm={3} md={3} lg={3} xl={3} key={pelicula.id}>
+                  <CardPelicula pelicula={pelicula} />
+                </Grid>
+              </Zoom>
+            ))}
+          </Grid>
+        </Box>
+      )}
+    </Box>
+  );
 }
 
 function formatDropdownData(data) {
-    return map(data, (item) => ({
-        key: item.id,
-        text: item.nombre,
-        value: item.id
-    }));
+  return map(data, (item) => ({
+    key: item.id,
+    text: item.nombre,
+    value: item.id,
+  }));
 }
