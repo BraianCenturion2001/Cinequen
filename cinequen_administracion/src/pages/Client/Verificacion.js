748801309d323593efcb8fea3029080f74f586ad
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-12-05 16:41:28 -0300

    cambios verificacion

--- cinequen_administracion/src/pages/Client/Verificacion.js
+++ cinequen_administracion/src/pages/Client/Verificacion.js
@@ -1,68 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import { Loader } from "semantic-ui-react"
-import { useParams } from 'react-router-dom';
-import { useUser } from "../../hooks"
+import React, { useEffect, useState } from "react";
+import { Loader } from "semantic-ui-react";
+import { useParams } from "react-router-dom";
+import { useUser } from "../../hooks";
 import { Box, Button, Typography, Grid } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 export function Verificacion() {
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const { validateUser, loading } = useUser();
-    const [retener, setRetener] = useState(true)
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { validateUser, loading } = useUser();
+  const [retener, setRetener] = useState(true);
 
-    useEffect(() => {
-        validateUser(id)
-        document.title = 'Verificarme';
-    }, [])
+  useEffect(() => {
+    validateUser(id);
+    document.title = "Verificarme";
+  }, []);
 
-    useEffect(() => {
-        setInterval(() => {
-            setRetener(false);
-        }, 3000);
-    }, [loading])
+  useEffect(() => {
+    setInterval(() => {
+      setRetener(false);
+    }, 3000);
+  }, [loading]);
 
-    const navigateToLogin = () => {
-        navigate("/login");
-    };
+  const navigateToLogin = () => {
+    navigate("/login");
+  };
 
-    return (
-        <Box
-            sx={{
-                height: '200px',
-                width: '250px',
-                backgroundColor: 'white',
-                borderRadius: 1,
-                margin: '0 auto',
-                marginTop: '25vh',
-            }}
-        >
-            {
-                retener ? (
-                    <Loader active inline="centered" >
-                        Verificando usuario...
-                    </Loader>
-                ) : (
-                    <Grid container spacing={2}>
-                        <Grid item >
-                            <Typography variant="h5" gutterBottom>
-                                Verificación completada
-                            </Typography>
-                        </Grid>
-                        <Grid item >
-                            <Button
-                                type="submit"
-                                variant="outlined"
-                                color="success"
-                                fullWidth
-                                onClick={navigateToLogin}
-                            >
-                                Ir a Log-In
-                            </Button>
-                        </Grid>
-                    </Grid>
-                )
-            }
-        </Box >
-    )
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        width: "50%",
+        margin: "0 auto",
+        marginTop: "25px",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "rgba(255, 255, 255, 0.8)",
+        borderRadius: 1,
+        height: "400px",
+      }}
+    >
+      {retener ? (
+        <Loader active size="big" inline="centered">
+          Verificando usuario...
+        </Loader>
+      ) : (
+        <>
+          <Typography variant="h5" color="textPrimary" gutterBottom>
+            Verificación completada
+          </Typography>
+          <Button
+            type="submit"
+            variant="contained"
+            color="success"
+            onClick={navigateToLogin}
+          >
+            Ir a Iniciar Sesión
+          </Button>
+        </>
+      )}
+    </Box>
+  );
 }
