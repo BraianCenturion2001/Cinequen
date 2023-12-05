0a77b775e06d59e2de306236c428a7a4eaaa3007
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-12-05 12:43:00 -0300

    Tabla canjes mejoras visuales

--- cinequen_administracion/src/components/Client/TablaCanjes/TablaCanjes.js
+++ cinequen_administracion/src/components/Client/TablaCanjes/TablaCanjes.js
@@ -7,15 +7,17 @@ import {
   TableHead,
   TableRow,
   Paper,
-  Button,
+  Box,
   Typography,
 } from "@mui/material";
 import { map } from "lodash";
 import { Image } from "semantic-ui-react";
 import dayjs from "dayjs";
+import { useAuth } from "../../../hooks"
 
 export function TablaCanjes(props) {
   const { canjes } = props;
+  const { auth } = useAuth();
 
   const formatearFecha = (fecha) => {
     const fechaFormateada = dayjs(fecha).format("MMM DD, YYYY");
@@ -40,48 +42,123 @@ export function TablaCanjes(props) {
           />
         </TableCell>
         <TableCell>
-          <Typography variant="h6">- {canje.puntos_restados}</Typography>
+          <Typography variant="h6">
+            <Box
+              sx={{
+                marginLeft: '8px',
+                backgroundColor: "yellow",
+                borderRadius: "15px",
+                width: "100px",
+                padding: "7px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <i
+                className="fa-duotone fa-ticket"
+                style={{
+                  marginRight: "8px",
+                  "--fa-primary-color": "#0f0f0f",
+                  "--fa-secondary-color": "#e31616",
+                  "--fa-secondary-opacity": 0.8,
+                }}
+              ></i>
+              - {canje.puntos_restados}
+            </Box></Typography>
         </TableCell>
       </TableRow>
     ));
   };
 
   return (
-    <TableContainer
-      component={Paper}
-      style={{
-        width: "90%",
-        margin: "0 auto",
-        marginTop: "20px",
-        marginBottom: "20px",
-      }}
-    >
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center" colSpan={4}>
-              <Typography variant="h2">Historial de Canjes</Typography>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Fecha de Canje</TableCell>
-            <TableCell>Producto</TableCell>
-            <TableCell>Imagen Producto</TableCell>
-            <TableCell>Puntos Coste</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {canjes.length > 0 ? (
-            renderFilasTabla(canjes)
-          ) : (
+    <>
+      <Box
+        sx={{
+          width: "90%",
+          margin: "0 auto",
+          backgroundColor: "rgba(255, 255, 255, 0.8)",
+          borderRadius: 1,
+          mt: "20px",
+          mb: "20px",
+          padding: "15px",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          variant="h6"
+          gutterBottom
+          sx={{
+            marginRight: "15px",
+            paddingTop: "5px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          Mis puntos actualmente:{" "}
+          <Box
+            sx={{
+              marginLeft: '8px',
+              backgroundColor: "yellow",
+              borderRadius: "15px",
+              width: "100px",
+              padding: "7px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <i
+              className="fa-duotone fa-ticket"
+              style={{
+                marginRight: "8px",
+                "--fa-primary-color": "#0f0f0f",
+                "--fa-secondary-color": "#e31616",
+                "--fa-secondary-opacity": 0.8,
+              }}
+            ></i>
+            {auth.me.puntos}
+          </Box>
+        </Typography>
+      </Box>
+      <TableContainer
+        component={Paper}
+        style={{
+          width: "90%",
+          margin: "0 auto",
+          marginTop: "20px",
+          marginBottom: "20px",
+        }}
+      >
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
             <TableRow>
-              <TableCell align="center" colSpan={6}>
-                Sin entradas registradas.
+              <TableCell align="center" colSpan={4}>
+                <Typography variant="h3">Historial de Canjes</Typography>
               </TableCell>
             </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+            <TableRow>
+              <TableCell>Fecha de Canje</TableCell>
+              <TableCell>Producto</TableCell>
+              <TableCell>Imagen Producto</TableCell>
+              <TableCell>Puntos Coste</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {canjes.length > 0 ? (
+              renderFilasTabla(canjes)
+            ) : (
+              <TableRow>
+                <TableCell align="center" colSpan={6}>
+                  Sin entradas registradas.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
