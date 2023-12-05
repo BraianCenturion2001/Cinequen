982a29b97123e2426eb4382cf3d187f59f6737e1
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-12-05 09:06:13 -0300

    Funciones ordenadas por sala tipo, idioma y formato

--- cinequen_administracion/src/components/Client/AccordionFunciones/AccordionFunciones.js
+++ cinequen_administracion/src/components/Client/AccordionFunciones/AccordionFunciones.js
@@ -1,79 +1,87 @@
-import React from 'react';
+import React from "react";
 import "./AccordionFunciones.scss";
-import { Box, Accordion, AccordionSummary, AccordionDetails, Typography, Button } from '@mui/material';
-import { Icon } from 'semantic-ui-react';
-import { map, groupBy } from 'lodash';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from "../../../hooks"
-import { toast } from 'react-toastify';
+import {
+  Box,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+  Button,
+} from "@mui/material";
+import { Icon } from "semantic-ui-react";
+import { map, groupBy } from "lodash";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../../hooks";
+import { toast } from "react-toastify";
 
 export function AccordionFunciones(props) {
-    const { gruposFunciones, expanded, handleChange } = props;
-    const navigate = useNavigate();
-    const { auth } = useAuth();
+  const { gruposFunciones, expanded, handleChange } = props;
+  console.log(gruposFunciones);
+  const navigate = useNavigate();
+  const { auth } = useAuth();
 
-    const handleButtonClick = (funcionId) => {
-        if (!auth) {
-            toast.error('Para comprar entradas debes iniciar sesión!');
-        } else {
-            navigate(`/funcion/compra/${funcionId}`);
-        }
-    };
-
-    function renderFunciones(funciones) {
-        return (
-            <>
-                {map(funciones, (funcion) => (
-                    <Button
-                        key={funcion.id}
-                        variant="outlined"
-                        sx={{ marginRight: '10px' }}
-                        onClick={() => handleButtonClick(funcion.id)}
-                    >
-                        {funcion.hora_inicio.substring(0, 5)}
-                    </Button>
-                ))}
-            </>
-        );
+  const handleButtonClick = (funcionId) => {
+    if (!auth) {
+      toast.error("Para comprar entradas debes iniciar sesión!");
+    } else {
+      navigate(`/funcion/compra/${funcionId}`);
     }
+  };
 
+  function renderFunciones(funciones) {
     return (
-        <>
-            {map(gruposFunciones, (grupo) => {
-                const salasPorEstablecimiento = groupBy(grupo.funciones, 'sala_data.id');
-                return (
-                    <Accordion
-                        sx={{ marginBottom: '20px' }}
-                        key={grupo.establecimiento.id}
-                        expanded={expanded === grupo.establecimiento.id}
-                        onChange={handleChange(grupo.establecimiento.id)}
-                    >
-                        <AccordionSummary
-                            expandIcon={<Icon name="angle down" />}
-                            aria-controls="panel1bh-content"
-                            id="panel1bh-header"
-                        >
-                            <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                                {grupo.establecimiento.nombre}
-                            </Typography>
-                            <Typography sx={{ color: 'text.secondary' }}>
-                                {grupo.establecimiento.direccion}
-                            </Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            {map(salasPorEstablecimiento, (funciones, salaId) => {
-                                const sala = funciones[0].sala_data;
-                                return (
-                                    <Box key={salaId} sx={{ marginBottom: '20px' }}>
-                                        <Typography variant="h6" sx={{ marginBottom: '10px' }}>{sala.tipo}</Typography>
-                                        {renderFunciones(funciones)}
-                                    </Box>
-                                );
-                            })}
-                        </AccordionDetails>
-                    </Accordion>
-                );
-            })}
-        </>
+      <>
+        {map(funciones, (funcion) => (
+          <Button
+            key={funcion.id}
+            variant="outlined"
+            sx={{ marginRight: "10px" }}
+            onClick={() => handleButtonClick(funcion.id)}
+          >
+            {funcion.hora_inicio.substring(0, 5)}
+          </Button>
+        ))}
+      </>
     );
+  }
+
+  return (
+    <>
+      {map(gruposFunciones, (grupo) => {
+        return (
+          <Accordion
+            sx={{ marginBottom: "20px" }}
+            key={grupo.establecimiento.id}
+            expanded={expanded === grupo.establecimiento.id}
+            onChange={handleChange(grupo.establecimiento.id)}
+          >
+            <AccordionSummary
+              expandIcon={<Icon name="angle down" />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                {grupo.establecimiento.nombre}
+              </Typography>
+              <Typography sx={{ color: "text.secondary" }}>
+                {grupo.establecimiento.direccion}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              {map(grupo.funcionesFormato, (funcionesSalaTipo, index) => {
+                return (
+                  <Box key={index} sx={{ marginBottom: "20px" }}>
+                    <Typography variant="h6" sx={{ marginBottom: "10px" }}>
+                      {funcionesSalaTipo.formatoIdiomaSalaTipo}
+                    </Typography>
+                    {renderFunciones(funcionesSalaTipo.funciones)}
+                  </Box>
+                );
+              })}
+            </AccordionDetails>
+          </Accordion>
+        );
+      })}
+    </>
+  );
 }
--- cinequen_administracion/src/pages/Client/FuncionesPelicula.js
+++ cinequen_administracion/src/pages/Client/FuncionesPelicula.js
@@ -1,134 +1,170 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useFuncion } from "../../hooks";
-import { useParams } from 'react-router-dom';
-import { Icon, Loader } from 'semantic-ui-react';
-import { Button, Box, Typography } from '@mui/material';
-import { AccordionFunciones } from "../../components/Client"
-import { map, uniqBy } from 'lodash';
-import dayjs from 'dayjs';
+import { useParams } from "react-router-dom";
+import { Icon, Loader } from "semantic-ui-react";
+import { Button, Box, Typography } from "@mui/material";
+import { AccordionFunciones } from "../../components/Client";
+import { map, uniqBy } from "lodash";
+import dayjs from "dayjs";
 
 export function FuncionesPelicula() {
-    const { id } = useParams();
-    const { loading, funciones, getFuncionesPelicula } = useFuncion();
-    const [expanded, setExpanded] = useState(false);
-    const [nombrePelicula, setNombrePelicula] = useState('');
-    const [selectedFecha, setSelectedFecha] = useState('');
-    const [gruposFunciones, setGruposFunciones] = useState([]);
+  const { id } = useParams();
+  const { loading, funciones, getFuncionesPelicula } = useFuncion();
+  const [expanded, setExpanded] = useState(false);
+  const [nombrePelicula, setNombrePelicula] = useState("");
+  const [selectedFecha, setSelectedFecha] = useState("");
+  const [gruposFunciones, setGruposFunciones] = useState([]);
 
-    const handleChange = (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false);
-    };
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
-    useEffect(() => {
-        getFuncionesPelicula(id);
-        document.title = 'Ver Funciones';
-    }, []);
+  useEffect(() => {
+    getFuncionesPelicula(id);
+    document.title = "Ver Funciones";
+  }, []);
 
-    useEffect(() => {
-        if (!loading) {
-            if (funciones.length > 0) {
-                //console.log(funciones)
-                setNombrePelicula(funciones[0].pelicula_data.nombre);
-                const gruposOrdenados = ordenarFuncionesPorFechaYEstablecimiento(funciones);
-                setGruposFunciones(gruposOrdenados);
+  useEffect(() => {
+    if (!loading) {
+      if (funciones.length > 0) {
+        //console.log(funciones)
+        setNombrePelicula(funciones[0].pelicula_data.nombre);
+        const gruposOrdenados =
+          ordenarFuncionesPorFechaYEstablecimiento(funciones);
+        setGruposFunciones(gruposOrdenados);
 
-                // Seleccionar la primera fecha y mostrar el acordeón correspondiente
-                const primeraFecha = uniqBy(funciones, 'fecha')[0].fecha;
-                handleFechaClick(primeraFecha); // Seleccionar la primera fecha al cargar el componente
-            }
-        }
-    }, [loading, funciones]);
+        // Seleccionar la primera fecha y mostrar el acordeón correspondiente
+        const primeraFecha = uniqBy(funciones, "fecha")[0].fecha;
+        handleFechaClick(primeraFecha); // Seleccionar la primera fecha al cargar el componente
+      }
+    }
+  }, [loading, funciones]);
 
-    const ordenarFuncionesPorFechaYEstablecimiento = (funciones) => {
-        const gruposPorFecha = map(uniqBy(funciones, 'fecha'), (funcion) => ({
-            fecha: dayjs(funcion.fecha).format('MMM DD'),
-            grupos: [],
-        }));
+  const ordenarFuncionesPorFechaYEstablecimiento = (funciones) => {
+    const gruposPorFecha = map(uniqBy(funciones, "fecha"), (funcion) => ({
+      fecha: dayjs(funcion.fecha).format("MMM DD"),
+      grupos: [],
+    }));
 
-        funciones.forEach((funcion) => {
-            const grupoFecha = gruposPorFecha.find((grupo) => grupo.fecha === dayjs(funcion.fecha).format('MMM DD'));
-            const establecimiento = funcion.sala_data.establecimiento_data;
+    funciones.forEach((funcion) => {
+      const grupoFecha = gruposPorFecha.find(
+        (grupo) => grupo.fecha === dayjs(funcion.fecha).format("MMM DD")
+      );
+      const establecimiento = funcion.sala_data.establecimiento_data;
+      const sala_tipo = funcion.sala_data.tipo;
 
-            let grupoEstablecimiento = grupoFecha.grupos.find(
-                (grupo) => grupo.establecimiento.id === establecimiento.id
-            );
+      let grupoEstablecimiento = grupoFecha.grupos.find(
+        (grupo) => grupo.establecimiento.id === establecimiento.id
+      );
 
-            if (!grupoEstablecimiento) {
-                grupoEstablecimiento = {
-                    establecimiento: establecimiento,
-                    funciones: [],
-                };
-                grupoFecha.grupos.push(grupoEstablecimiento);
-            }
+      if (!grupoEstablecimiento) {
+        grupoEstablecimiento = {
+          establecimiento: establecimiento,
+          funcionesFormato: [],
+        };
+        grupoFecha.grupos.push(grupoEstablecimiento);
+      }
 
-            grupoEstablecimiento.funciones.push(funcion);
-        });
+      const formatoIdiomaSalaTipo = `${sala_tipo} ${funcion.formato} ${funcion.idioma}`;
+      let grupoFormato = grupoEstablecimiento.funcionesFormato.find(
+        (grupo) => grupo.formatoIdiomaSalaTipo === formatoIdiomaSalaTipo
+      );
 
-        gruposPorFecha.forEach((grupoFecha) => {
-            grupoFecha.grupos.sort((a, b) => {
-                const nombreEstablecimientoA = a.establecimiento.nombre.toLowerCase();
-                const nombreEstablecimientoB = b.establecimiento.nombre.toLowerCase();
-                return nombreEstablecimientoA.localeCompare(nombreEstablecimientoB);
-            });
-        });
+      if (!grupoFormato) {
+        grupoFormato = {
+          formatoIdiomaSalaTipo,
+          formato: funcion.formato,
+          idioma: funcion.idioma,
+          salaTipo: sala_tipo,
+          funciones: [],
+        };
+        grupoEstablecimiento.funcionesFormato.push(grupoFormato);
+      }
 
-        return gruposPorFecha;
-    };
+      grupoFormato.funciones.push(funcion);
+    });
 
-    const handleFechaClick = (fecha) => {
-        const fechaFormateada = dayjs(fecha).format('MMM DD');
-        setSelectedFecha(fechaFormateada);
-    };
+    gruposPorFecha.forEach((grupoFecha) => {
+      grupoFecha.grupos.sort((a, b) => {
+        const nombreEstablecimientoA = a.establecimiento.nombre.toLowerCase();
+        const nombreEstablecimientoB = b.establecimiento.nombre.toLowerCase();
+        return nombreEstablecimientoA.localeCompare(nombreEstablecimientoB);
+      });
+    });
 
-    const fechasDisponibles = uniqBy(funciones, 'fecha')
-        .map((funcion) => {
-            const fechaFormateada = dayjs(funcion.fecha).format('MMM DD');
-            return fechaFormateada;
-        })
-        .sort((a, b) => dayjs(a).toDate() - dayjs(b).toDate());
+    return gruposPorFecha;
+  };
 
-    return (
-        <>
-            <Box sx={{ width: '95%', margin: '0 auto', backgroundColor: 'rgba(255, 255, 255, 0.8)', borderRadius: 1, mt: '20px', mb: '200px', padding: '30px' }}>
-                {loading ? (
-                    <Loader active inline="centered">
-                        Cargando
-                    </Loader>
-                ) : (
-                    funciones.length > 0 ? (
-                        <>
-                            <Typography variant="h3" align="center">Funciones para {nombrePelicula}</Typography>
-                            <Box>
-                                {fechasDisponibles.map((fecha) => (
-                                    <Button
-                                        key={fecha}
-                                        size="large"
-                                        onClick={() => handleFechaClick(fecha)}
-                                        variant={fecha === selectedFecha ? 'contained' : 'outlined'}
-                                        sx={{
-                                            margin: '1rem',
-                                            alignItems: 'center',
-                                        }}
-                                    >
-                                        <Icon name='calendar alternate outline' size='big' /> {fecha}
-                                    </Button>
-                                ))}
-                            </Box>
-                            <Box sx={{ width: '500px' }}>
-                                {selectedFecha && (
-                                    <AccordionFunciones
-                                        handleChange={handleChange}
-                                        expanded={expanded}
-                                        gruposFunciones={gruposFunciones.find((grupo) => grupo.fecha === selectedFecha).grupos}
-                                    />
-                                )}
-                            </Box>
-                        </>
-                    ) : (
-                        <Typography variant="h3" align="center">Sin funciones disponibles para esta semana.</Typography>
-                    )
-                )}
+  const handleFechaClick = (fecha) => {
+    const fechaFormateada = dayjs(fecha).format("MMM DD");
+    setSelectedFecha(fechaFormateada);
+  };
+
+  const fechasDisponibles = uniqBy(funciones, "fecha")
+    .map((funcion) => {
+      const fechaFormateada = dayjs(funcion.fecha).format("MMM DD");
+      return fechaFormateada;
+    })
+    .sort((a, b) => dayjs(a).toDate() - dayjs(b).toDate());
+
+  return (
+    <>
+      <Box
+        sx={{
+          width: "95%",
+          margin: "0 auto",
+          backgroundColor: "rgba(255, 255, 255, 0.8)",
+          borderRadius: 1,
+          mt: "20px",
+          mb: "200px",
+          padding: "30px",
+        }}
+      >
+        {loading ? (
+          <Loader active inline="centered">
+            Cargando
+          </Loader>
+        ) : funciones.length > 0 ? (
+          <>
+            <Typography variant="h3" align="center">
+              Funciones para {nombrePelicula}
+            </Typography>
+            <Box>
+              {fechasDisponibles.map((fecha) => (
+                <Button
+                  key={fecha}
+                  size="large"
+                  onClick={() => handleFechaClick(fecha)}
+                  variant={fecha === selectedFecha ? "contained" : "outlined"}
+                  sx={{
+                    margin: "1rem",
+                    alignItems: "center",
+                  }}
+                >
+                  <Icon name="calendar alternate outline" size="big" /> {fecha}
+                </Button>
+              ))}
             </Box>
-        </>
-    );
+            <Box sx={{ width: "500px" }}>
+              {selectedFecha && (
+                <AccordionFunciones
+                  handleChange={handleChange}
+                  expanded={expanded}
+                  gruposFunciones={
+                    gruposFunciones.find(
+                      (grupo) => grupo.fecha === selectedFecha
+                    ).grupos
+                  }
+                />
+              )}
+            </Box>
+          </>
+        ) : (
+          <Typography variant="h3" align="center">
+            Sin funciones disponibles para esta semana.
+          </Typography>
+        )}
+      </Box>
+    </>
+  );
 }
